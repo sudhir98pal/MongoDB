f7e13095117e9b6b633e330386bc90fb43ed0e8d
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-09-16 20:59:21 +0530

    mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -28,13 +28,41 @@ type:Number
     }
 })
 
-const myself=new User({name:"Sudhir Pal",age:"miki"});
-myself.save().then((myself)=>
+const task=mongoose.model(
+    'task',
+            {
+                name:
+                {
+                     type:String
+
+                },
+                description:
+                {
+                    type:String
+                }
+            }
+
+)
+const mytask=new task({name:"coding",description:'this is the main task to be done during interview preparation for college palcements'})
+mytask.save().then((result)=>
 {
-    console.log(myself);
-    // myself is a varible you change its name to other like result 
+console.log(chalk.green('task saved successfully'));
 
 }).catch((error)=>
 {
-console.log('Error '+error);
+    console.log('Unable to save this task');
+    console.log(chalk.red('Error'+error));
+  
 })
+
+// const myself=new User({name:"Sudhir Pal",age:"miki"});
+// myself.save().then((myself)=>
+// {
+//     console.log(myself);
+//     // myself is a varible you change its name to other like result 
+
+// }).catch((error)=>
+// {
+// console.log('Error '+error);
+// })
+
