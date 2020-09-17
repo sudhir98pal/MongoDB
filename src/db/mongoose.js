3fc8d85605ed2c2f04c5b4c44e83b21d68ee2b07
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-09-17 20:54:33 +0530

    Exploring Validator for Mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -21,48 +21,65 @@ const User=mongoose.model('User',
 {
     name:
     {
-type:String
+type:String,
+require:true,
+validate(name)
+{
+    if(name.length<=1)
+    {
+        throw new Error('Please provide valide name')
+    }
+}
     },age:
     {
-type:Number
+type:Number,
+require:true,
+// if require is true ,then file entering value ,if not provide you will see error
+validate(age)
+{
+if(age<=0)
+{
+    throw new Error('Please provide valide age')
+}
+}
     }
 })
 
-const task=mongoose.model(
-    'task',
-            {
-                name:
-                {
-                     type:String
+// const task=mongoose.model(
+//     'task',
+//             {
+//                 name:
+//                 {
+//                      type:String
 
-                },
-                description:
-                {
-                    type:String
-                }
-            }
+//                 },
+//                 description:
+//                 {
+//                     type:String
+//                 }
+//             }
 
-)
-const mytask=new task({name:"coding",description:'this is the main task to be done during interview preparation for college palcements'})
-mytask.save().then((result)=>
-{
-console.log(chalk.green('task saved successfully'));
-
-}).catch((error)=>
-{
-    console.log('Unable to save this task');
-    console.log(chalk.red('Error'+error));
-  
-})
-
-// const myself=new User({name:"Sudhir Pal",age:"miki"});
-// myself.save().then((myself)=>
+// )
+// const mytask=new task({name:"coding",description:'this is the main task to be done during interview preparation for college palcements'})
+// mytask.save().then((result)=>
 // {
-//     console.log(myself);
-//     // myself is a varible you change its name to other like result 
+// console.log(chalk.green('task saved successfully'));
 
 // }).catch((error)=>
 // {
-// console.log('Error '+error);
+//     console.log('Unable to save this task');
+//     console.log(chalk.red('Error'+error));
+  
 // })
+
+const newuser=new User({name:'Nandlal pal',age:56});
+newuser.save().then((myself)=>
+{
+    console.log(myself);
+    // myself is a varible you change its name to other like result 
+
+}).catch((error)=>
+{
+console.log('Error '+error);
+})
 
