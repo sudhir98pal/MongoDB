8cd16260cc120b99e2c225b4f610b1de29047c50
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-09-24 17:11:58 +0530

    mongoose model explored

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -20,10 +20,12 @@ mongoose.connect(
 
 const User=mongoose.model('User',
 {
+
+
     name:
     {
 type:String,
-require:true,
+required:true,
 trim:true,// it is used to trim the trailing space from the end of the string
 validate(name)
 {
@@ -32,11 +34,14 @@ validate(name)
         throw new Error('Please provide valide name')
     }
 }
-    },age:
+    }
+    
+    
+    ,age:
     {
 type:Number,
-require:true,
-// if require is true ,then file entering value ,if not provide you will see error
+required:true,
+// if required is true ,then file entering value ,if not provide you will see error
 validate(age)
 {
 if(age<=0)
@@ -45,10 +50,12 @@ if(age<=0)
 }
 }
     },
+
+
     email:
     {
         type:String,
-        require:true,
+        required:true,
         trim:true,
         lowercase:true,
         validate(email)
@@ -56,43 +63,81 @@ if(age<=0)
             if(!validator.isEmail(email))
             {
 throw new Error('Please Provide Valid Email Addresh');
-return;
+
             }
         }
+    },
+
+
+
+
+    password:
+    {
+            type:String,
+            required:true,
+            trim:true,
+            lowercase:true,
+            validate(pass)
+            {
+                if(pass.includes("password"))
+                {
+                        throw new Error('Your Password is Invalid it contains key Password itself');
+                }
+                else{
+
+
+                    if(pass.length<=6)
+                    {
+                        throw new Error('Your Password is to too short Please Enter the Password of size greater than equal to 6')
+                    }
+                }
+
+
+            }
+
+
+
+
     }
 
 })
 
-// const task=mongoose.model(
-//     'task',
-//             {
-//                 name:
-//                 {
-//                      type:String,
-                        // trim:true
+const task=mongoose.model(
+    'task',
+            {
+                description:
+                {
+                    type:String,
+                    trim:true,
+                    required:true
+                },
+                completed:
+                {
+                     type:Boolean,
+                     default:false
+                        
 
-//                 },
-//                 description:
-//                 {
-//                     type:String,
-//trim:true
-//                 }
-//             }
+                },
+               
+            }
 
-// )
-// const mytask=new task({name:"coding",description:'this is the main task to be done during interview preparation for college palcements'})
-// mytask.save().then((result)=>
-// {
-// console.log(chalk.green('task saved successfully'));
+)
+const mytask=new task({
+    description:' Breakfast  ',
+    completed:true
+})
+mytask.save().then((result)=>
+{
+console.log(chalk.green('task saved successfully'));
 
-// }).catch((error)=>
-// {
-//     console.log('Unable to save this task');
-//     console.log(chalk.red('Error'+error));
+}).catch((error)=>
+{
+    console.log('Unable to save this task');
+    console.log(chalk.red('Error'+error));
   
-// })
+})
 
-// const newuser=new User({name:'Ram',age:23,email:'ramu@xxx.gmail.com'});
+// const newuser=new User({name:'Saadhu',age:56,email:'mysadhuhu@gmail.com',password:'youcannottfindmypass   '});
 // newuser.save().then((myself)=>
 // {
 //     console.log(myself);
